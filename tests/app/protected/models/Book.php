9b5b95009a9e25f7cdceef8e5b699e59effde15a
Fredrik Wollsén <fredrik@neam.se>
2013-11-15 22:41:12 +0200

    Pristine generated models based on unmodified database

---
+++ tests/app/protected/models/Book.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * This is the model class for table "book".
+ *
+ * The followings are the available columns in table 'book':
+ * @property string $id
+ * @property string $title
+ * @property string $slug
+ * @property string $image_id
+ * @property string $created
+ * @property string $modified
+ *
+ * The followings are the available model relations:
+ * @property Image $image
+ * @property Chapter[] $chapters
+ */
+class Book extends CActiveRecord
+{
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @param string $className active record class name.
+	 * @return Book the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'book';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('id, image_id', 'required'),
+			array('id, image_id', 'length', 'max'=>20),
+			array('title, slug', 'length', 'max'=>255),
+			array('created, modified', 'safe'),
+			// The following rule is used by search().
+			// Please remove those attributes that should not be searched.
+			array('id, title, slug, image_id, created, modified', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'image' => array(self::BELONGS_TO, 'Image', 'image_id'),
+			'chapters' => array(self::HAS_MANY, 'Chapter', 'book_id'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'title' => 'Title',
+			'slug' => 'Slug',
+			'image_id' => 'Image',
+			'created' => 'Created',
+			'modified' => 'Modified',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id,true);
+		$criteria->compare('title',$this->title,true);
+		$criteria->compare('slug',$this->slug,true);
+		$criteria->compare('image_id',$this->image_id,true);
+		$criteria->compare('created',$this->created,true);
+		$criteria->compare('modified',$this->modified,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+}
---
+++ tests/app/protected/models/Chapter.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * This is the model class for table "chapter".
+ *
+ * The followings are the available columns in table 'chapter':
+ * @property string $id
+ * @property string $title
+ * @property string $slug
+ * @property string $book_id
+ * @property string $created
+ * @property string $modified
+ *
+ * The followings are the available model relations:
+ * @property Book $book
+ */
+class Chapter extends CActiveRecord
+{
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @param string $className active record class name.
+	 * @return Chapter the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'chapter';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('id, book_id', 'required'),
+			array('id, book_id', 'length', 'max'=>20),
+			array('title, slug', 'length', 'max'=>255),
+			array('created, modified', 'safe'),
+			// The following rule is used by search().
+			// Please remove those attributes that should not be searched.
+			array('id, title, slug, book_id, created, modified', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'book' => array(self::BELONGS_TO, 'Book', 'book_id'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'title' => 'Title',
+			'slug' => 'Slug',
+			'book_id' => 'Book',
+			'created' => 'Created',
+			'modified' => 'Modified',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id,true);
+		$criteria->compare('title',$this->title,true);
+		$criteria->compare('slug',$this->slug,true);
+		$criteria->compare('book_id',$this->book_id,true);
+		$criteria->compare('created',$this->created,true);
+		$criteria->compare('modified',$this->modified,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+}
